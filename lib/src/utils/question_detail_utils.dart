420cd31b3b5ad2aa0dc2eb64149044de3183b54c
Sangmin <jeonsm0404@gmail.com>
2024-11-20 10:43:47 +0900

    fix: resolve nullable error on question detail page
    add loading widget, until load the question

--- lib/src/utils/question_detail_utils.dart
+++ lib/src/utils/question_detail_utils.dart
@@ -1,11 +1,30 @@
-import 'package:code_ground/src/services/database/datas/tier_data.dart';
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
+import 'package:collection/collection.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
 import 'package:code_ground/src/view_models/progress_view_model.dart';
 import 'package:code_ground/src/view_models/question_view_model.dart';
+import 'package:code_ground/src/services/database/datas/tier_data.dart';
 import 'package:code_ground/src/services/database/datas/question_data.dart';
-import 'package:collection/collection.dart'; // ListEquality 사용을 위한 import
+
+void showLoading(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (_) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    },
+  );
+}
+
+/// 로딩 스피너를 숨기는 유틸리티 함수
+void hideLoading(BuildContext context) {
+  Navigator.of(context, rootNavigator: true).pop();
+}
 
 bool verifyAnswer(dynamic userAnswer, QuestionData question) {
   if (question.questionType != 'Sequencing') {
@@ -23,59 +42,63 @@ bool verifyAnswer(dynamic userAnswer, QuestionData question) {
   return ListEquality().equals(userAnswerList, correctAnswerList);
 }
 
-void showAnswerResult(BuildContext context, bool isCorrect) {
+void showAnswerResult(BuildContext context, bool isCorrect) async {
   final progressViewModel =
       Provider.of<ProgressViewModel>(context, listen: false);
   final questionViewModel =
       Provider.of<QuestionViewModel>(context, listen: false);
   final question = questionViewModel.selectedQuestion;
 
-  // 정답 여부 표시
-  Fluttertoast.showToast(
-    msg: isCorrect ? "Correct!" : "Wrong! Try Again.",
-    toastLength: Toast.LENGTH_SHORT,
-    gravity: ToastGravity.BOTTOM,
-    backgroundColor: isCorrect ? Colors.green : Colors.red,
-    textColor: Colors.white,
-  );
-
-  // 디버깅 메시지: 문제 상태 확인
-  final bool isAlreadyAnswered =
-      progressViewModel.progressData?.questionState[question?.questionId] ??
-          false;
-  debugPrint("Question ID: ${question!.questionId}");
-  debugPrint("Is Already Answered: $isAlreadyAnswered");
-  debugPrint("Question Tier: ${question.tier}");
-
-  if (isCorrect && !isAlreadyAnswered) {
-    // 점수 및 경험치 추가
-
-    final tier = Tier.getTierByName(question.tier!);
-
-    progressViewModel.addExp(tier!.bonusExp);
-    progressViewModel.addScore(tier.bonusScore);
-
-    // 경험치와 점수 상승치를 표시
+  if (question == null) {
     Fluttertoast.showToast(
-      msg:
-          "You've earned +${tier.bonusExp} EXP and +${tier.bonusScore} points!",
+      msg: "Error: Question not found.",
       toastLength: Toast.LENGTH_SHORT,
       gravity: ToastGravity.BOTTOM,
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+    );
+    return;
+  }
+
+  showLoading(context);
+
+  try {
+    Fluttertoast.showToast(
+      msg: isCorrect ? "Correct!" : "Wrong! Try Again.",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: isCorrect ? Colors.green : Colors.red,
       textColor: Colors.white,
     );
 
-    debugPrint(
-        "Added ${tier.bonusExp} EXP and ${tier.bonusScore} points for question ID: ${question.questionId}");
-    progressViewModel.updateQuestionState(question.questionId, true);
-  } else if (isCorrect && isAlreadyAnswered) {
-    // 이미 정답 처리된 경우 디버깅 메시지 출력
-    debugPrint(
-        "Question ID: ${question.questionId} has already been answered correctly.");
-  } else {
-    // 틀린 경우 상태 업데이트
-    progressViewModel.updateQuestionState(question.questionId, false);
-    debugPrint(
-        "Updated question state to 'false' for Question ID: ${question.questionId}");
+    final bool isAlreadyAnswered =
+        progressViewModel.progressData?.questionState[question.questionId] ??
+            false;
+
+    if (isCorrect && !isAlreadyAnswered) {
+      final tier = Tier.getTierByName(question.tier!);
+
+      progressViewModel.addExp(tier!.bonusExp);
+      progressViewModel.addScore(tier.bonusScore);
+
+      Fluttertoast.showToast(
+        msg:
+            "You've earned +${tier.bonusExp} EXP and +${tier.bonusScore} points!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+      );
+
+      progressViewModel.updateQuestionState(question.questionId, true);
+    } else if (isCorrect && isAlreadyAnswered) {
+      debugPrint("Question ID: ${question.questionId} already answered.");
+    } else {
+      progressViewModel.updateQuestionState(question.questionId, false);
+      debugPrint(
+          "Updated question state to 'false' for Question ID: ${question.questionId}");
+    }
+  } finally {
+    hideLoading(context);
   }
 }
