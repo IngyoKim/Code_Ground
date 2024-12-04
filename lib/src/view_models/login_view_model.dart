5573a8e29939e75637ca3ee395cacf9b7288a78a
HJ1263532 <osisland2918@gmail.com>
2024-12-04 17:39:45 +0900

    add: logic message

--- lib/src/view_models/login_view_model.dart
+++ lib/src/view_models/login_view_model.dart
@@ -7,6 +7,7 @@ import 'package:code_ground/src/services/auth/social_login.dart';
 import 'package:code_ground/src/services/auth/login_preference.dart';
 
 class LoginViewModel extends ChangeNotifier {
+  //로그인 타입을 저장하고 관리하는 클레스
   final LoginPreference _loginPreference = LoginPreference();
   SocialLogin? _socialLogin;
   User? user;
