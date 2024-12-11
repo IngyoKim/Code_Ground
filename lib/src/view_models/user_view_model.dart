04c96e8037e63f0080fb13220f59c33ec32d51de
IngyoKim <a58276976@gmail.com>
2024-12-11 12:02:01 +0900

    chore: change annotations

--- lib/src/view_models/user_view_model.dart
+++ lib/src/view_models/user_view_model.dart
@@ -16,7 +16,7 @@ class UserViewModel with ChangeNotifier {
 
   /// Hashes 'uid' to generate 12-digit unique friend code
   String generateFriendCode(String uid) {
-    final bytes = utf8.encode(uid); // UID를 바이트로 변환
+    final bytes = utf8.encode(uid); // Converting UID to Bytes
     final hash = sha256.convert(bytes); // SHA-256 해싱
     return hash.toString().substring(0, 12).toUpperCase(); // 상위 12자만 사용
   }
