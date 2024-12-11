6ce2dde0fae47b103b81a093bf0b0a9a469d61f3
HJ1263532 <osisland2918@gmail.com>
2024-12-11 15:09:26 +0900

    add: faq page informaations

--- lib/src/pages/app_info/faq_page.dart
+++ lib/src/pages/app_info/faq_page.dart
@@ -11,14 +11,13 @@ class _FAQPageState extends State<FAQPage> {
   /// FAQ Questions and Answers List
   final List<Map<String, String>> faqs = [
     {
-      'question': 'How do I reset my password?',
+      'question': '친구 추가는 어떻게 하나요?',
       'answer':
-          'To reset your password, go to the settings page and select "Reset Password". You will receive an email with a link to reset your password.',
+          'settings의 Your Info 부분 안의 친구코드를 친구에게 공유하세요.\nSocial Page의 friend에서 친구 코드를 통해 친구를 추가할 수 있습니다.',
     },
     {
-      'question': 'How can I contact support?',
-      'answer':
-          'You can contact support through the "Help" section in the app. Simply click on the "Contact Support" button.',
+      'question': 'Admin 권한을 어떻게 요청하나요?',
+      'answer': '/카페 링크 기입 예정\n 해당 카페에서 요청하실 수 있습니다.',
     },
     {
       'question': 'What is the app\'s privacy policy?',
@@ -26,9 +25,9 @@ class _FAQPageState extends State<FAQPage> {
           'Our privacy policy can be accessed through the "About" page in the app. It outlines how we handle your data and your privacy rights.',
     },
     {
-      'question': 'How do I update the app?',
+      'question': '문제 추가하는 방법은 무엇인가요?',
       'answer':
-          'App updates are available through your device\'s app store (Google Play or Apple App Store). Make sure to enable automatic updates or check for updates manually.',
+          '문제를 추가하기 위해선 admin권한을 얻어야 합니다.\nadmin권한에 대한 자세한 내용은 Help에 기재되어 있습니다.',
     },
     {
       'question': 'How can I give feedback?',
--- lib/src/pages/app_info/help_page.dart
+++ lib/src/pages/app_info/help_page.dart
@@ -59,7 +59,7 @@ class _HelpPageState extends State<HelpPage> {
               context,
               title: 'Tier Chart',
               subtitle:
-                  '*모든 티어는 score 기준으로 나뉩니다.\n브론즈: 0-499\n실버: 500-1999\n골드: 2000-4999\n플래티넘: 5000-9999\n다이아: 10000-29999\n마스터: 30000-',
+                  '*모든 티어는 score 기준으로 나뉩니다.\n브론즈: 0-499\n실버: 500-1999\n골드: 2000-4999\n플래티넘: 5000-9999\n다이아: 10000-29999\n마스터: 30000-49999\n그랜드마스터: 50000-',
               onTap: () {
                 /// Show dialog with customer support contact information
                 showDialog(
@@ -80,7 +80,7 @@ class _HelpPageState extends State<HelpPage> {
               context,
               title: 'Admin권한',
               subtitle:
-                  'Admin권한은 3단계로 나뉩니다.\n memder-문제 풀기만 가능\nOperatior-문제 풀기, 수정 가능\nMaster-문제 풀기, 수정, 생성 가능\nAdmin권한을 얻기 위해선 제작자들에게 메일 주세요.',
+                  'Admin권한은 3단계로 나뉩니다.\n memder-문제 풀기만 가능\nOperatior-문제 풀기, 수정 가능\nMaster-문제 풀기, 수정, 생성 가능',
               onTap: () {
                 /// Show dialog for sending feedback
                 showDialog(
