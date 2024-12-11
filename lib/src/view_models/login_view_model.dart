ec4b6c46428f3ac3d1aa8dd4fd7bdcde22fa22ef
sja <tlswldks12345@gmail.com>
2024-12-11 15:20:36 +0900

    chore:change annotations

--- lib/src/view_models/login_view_model.dart
+++ lib/src/view_models/login_view_model.dart
@@ -6,7 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:code_ground/src/services/auth/social_login.dart';
 import 'package:code_ground/src/services/auth/login_preference.dart';
 
-///login session
+//login session
 class LoginViewModel extends ChangeNotifier {
   final LoginPreference _loginPreference = LoginPreference();
   SocialLogin? _socialLogin;
@@ -24,7 +24,7 @@ class LoginViewModel extends ChangeNotifier {
   Future<void> setLoginType(SocialLogin socialLogin) async {
     _socialLogin = socialLogin;
 
-    /// Save the selected login type to preferences.
+    // Save the selected login type to preferences.
     await _loginPreference.setLoginType(socialLogin);
     notifyListeners();
   }
@@ -53,4 +53,4 @@ class LoginViewModel extends ChangeNotifier {
     }
   }
 }
-///login version
+//login version
