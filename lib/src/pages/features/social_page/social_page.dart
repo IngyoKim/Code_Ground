63542a758335c0625ace3671197e3b5980cb995f
YunhoJangg <eunyun2066@gmail.com>
2024-12-17 19:24:26 +0900

    feat: add invite icon to app bar when Friends tab is selected
    - display an invite icon in the app bar only when the Friends tab is active
    - implemented TabController to track and respond to tab selection changes
    - added functionality to share invite links via the invite icon button

--- lib/src/pages/features/social_page/social_page.dart
+++ lib/src/pages/features/social_page/social_page.dart
@@ -1,37 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:code_ground/src/pages/features/social_page/friends_page.dart';
 import 'package:code_ground/src/pages/features/social_page/ranking/ranking_page.dart';
+import 'package:code_ground/src/services/messaging/custom_url.dart';
+import 'package:code_ground/src/view_models/user_view_model.dart';
+import 'package:code_ground/src/services/messaging/kakao_messaging.dart';
 
-class SocialPage extends StatelessWidget {
+class SocialPage extends StatefulWidget {
   const SocialPage({super.key});
 
   @override
+  State<SocialPage> createState() => _SocialPageState();
+}
+
+class _SocialPageState extends State<SocialPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.addListener(_handleTabSelection);
+  }
+
+  void _handleTabSelection() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // 탭의 개수
-      child: Scaffold(
+    final KakaoMessaging kakaoMessaging = KakaoMessaging();
+    final userViewModel = context.watch<UserViewModel>();
+    final userData = userViewModel.currentUserData;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          title: const Text("Social"),
-          bottom: const TabBar(
-            indicatorColor: Colors.black,
-            labelColor: Colors.black,
-            unselectedLabelColor: Colors.grey,
-            tabs: [
-              Tab(text: "Ranking"),
-              Tab(text: "Friends"),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          physics: const NeverScrollableScrollPhysics(),
-          children: const [
-            RankingPage(),
-            RegistrateFriends(),
+        foregroundColor: Colors.black,
+        title: const Text("Social"),
+        actions: [
+          if (_tabController.index == 1) // Friends 탭이 선택되었을 때만 아이콘 표시
+            IconButton(
+              icon: const Icon(Icons.person_add),
+              onPressed: () async {
+                // 여기에 버튼 동작을 추가
+                final inviteUrl = await createCustomLink(userData!.uid);
+                await kakaoMessaging.shareContent(userData.nickname, inviteUrl);
+              },
+            ),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.black,
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.grey,
+          tabs: const [
+            Tab(text: "Ranking"),
+            Tab(text: "Friends"),
           ],
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          RankingPage(),
+          RegistrateFriends(),
+        ],
       ),
     );
   }
